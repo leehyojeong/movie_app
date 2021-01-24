95a96bc1922897b43031bbd37d656fba927d064f
이효정 <39904216+leehyojeong@users.noreply.github.com>
2021-01-24 22:34:57 +0900

    food map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+function Food({fav, picture}) {
+  return <div>
+    <h2>I like {fav}</h2>
+    <img src={picture}/>
+  </div>;
+}
+
+const foodIlike = [
+  {
+    id: 1,
+    name: "kim",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.RSRN70ESBp8mYHmq8Q8znQHaDt%26pid%3DApi&f=1"
+  },
+  {
+    id: 2,
+    name: "don",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2bPqskIIDjez0ZuvbApLjAHaEK%26pid%3DApi&f=1"
+  }
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodIlike.map((dish, idx)=>(
+      <Food fav={dish.name} picture={dish.image} key={idx}/>
+    ))}
+  </div>
   );
 }
 
